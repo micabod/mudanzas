995e6825cff6aabcfa94c103440e9512e63289cf
micabod <167199490+micabod@users.noreply.github.com>
2025-04-28 13:09:21 -0300

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float kgusuario;
+    public float km;
+    string tipovehiculo;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (kgusuario<100 && km < 1)
+        {
+            Debug.Log("error");
+        }
+        float cuentatipovehiculo = kgusuario * km;
+        if (cuentatipovehiculo <= 2000)
+        {
+            tipovehiculo = ("Utilitario");
+        }
+        if (cuentatipovehiculo<5000 && cuentatipovehiculo > 2000)
+        {
+            tipovehiculo = ("Pickup");
+        }
+        if (cuentatipovehiculo<10000 && cuentatipovehiculo > 5000)
+        {
+            tipovehiculo = ("Camion");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
